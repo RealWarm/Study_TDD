2b1d39e6f1d27423012c8387329fc70a1b761c15
RealWarm <wjdgns8946@naver.com>
2024-05-05 18:31:28 +0900

    2차 리팩토링

--- src/main/java/com/study/tdd/chap02/PasswordStrengthMeter.java
+++ src/main/java/com/study/tdd/chap02/PasswordStrengthMeter.java
@@ -5,12 +5,9 @@ public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
         int metCounts=0;
-        boolean lengthEnough = s.length() >=8;
-        if(lengthEnough) metCounts++;
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        if(containsNum) metCounts++;
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
-        if(containsUpp) metCounts++;
+        if(s.length() >=8) metCounts++;
+        if(meetsContainingNumberCriteria(s)) metCounts++;
+        if(meetsContainingUppercaseCriteria(s)) metCounts++;
 
         /*if(lengthEnough && !containsNum && !containsUpp){
             return  PasswordStrength.WEAK;
@@ -23,11 +20,6 @@ public class PasswordStrengthMeter {
         }*/
         if(metCounts==1) return PasswordStrength.WEAK;
         if(metCounts==2) return PasswordStrength.NORMAL;
-        if(!lengthEnough) {
-            return PasswordStrength.NORMAL;
-        }
-        if (!containsNum) return PasswordStrength.NORMAL;
-        if (!containsUpp) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
 
     }//meter
